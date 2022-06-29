341c5f9d3ccf41ac3b25d10544d7e20c492051d5
Dario Paladines <dariopaladines.23@gmail.com>
2022-06-28 22:00:38 -0500

    agregando nuevamente el gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 
 
-// if((!empty( $_SERVER['HTTP_X_FORWARDED_HOST'])) || (!empty( $_SERVER['HTTP_X_FORWARDED_FOR'])) ) {
-//     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-//     $_SERVER['HTTPS'] = 'on';
-// }
 
 //Evento con teclado
 const enter = document.getElementById("ciudad")
