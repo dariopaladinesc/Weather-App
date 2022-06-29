dfc848515a08b4fc971b2158480654af16dfa6f4
Dario Paladines <dariopaladines.23@gmail.com>
2022-06-28 21:23:23 -0500

    variables globales a las apikeys

--- src/api_key.js
+++ src/api_key.js
@@ -1,3 +1,3 @@
-const base_API = 'https://api.openweathermap.org/data/2.5/weather'
-const apiKey= '0b5d5d30a262ea49fab0b87b861d3f03'
+var base_API = 'https://api.openweathermap.org/data/2.5/weather'
+var apiKey= '0b5d5d30a262ea49fab0b87b861d3f03'
 
